786537abd48d302a8a7412119d862ec7d1fd30d8
Alex Hawk <alha@hms.se>
2020-04-07 11:52:55 -0400

    Add tag constants class

---
+++ src/com/hms_networks/americas/sc/taginfo/TagConstants.java
@@ -0,0 +1,30 @@
+package com.hms_networks.americas.sc.taginfo;
+
+/**
+ * Class for storing constants used in the taginfo package.
+ *
+ * @author HMS Networks, MU Americas Solution Center
+ * @since 1.0
+ */
+public class TagConstants {
+
+  /**
+   * Integer value used as a placeholder for uninitialized values.
+   */
+  public static final int UNINIT_INT_VAL = -1;
+
+  /**
+   * New line character used in EBD files/calls.
+   */
+  public static final int TAG_EBD_NEW_LINE = '\n';
+
+  /**
+   * Carriage return character used in EBD files/calls.
+   */
+  public static final int TAG_EBD_CARRIAGE_RETURN = '\r';
+
+  /**
+   * End of stream marker used in EBD files/calls.
+   */
+  public static final int TAG_EBD_END_OF_STREAM = -1;
+}
